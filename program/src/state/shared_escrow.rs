1d19404e4418c93c647d37c70910887ddb16c32e
Samuel Vanderwaal <samuel.vanderwaal@gmail.com>
2024-03-11 10:56:46 -0800

    run cargo fmt

--- program/src/state/shared_escrow.rs
+++ program/src/state/shared_escrow.rs
@@ -6,7 +6,7 @@ use anchor_lang::prelude::*;
 pub const SHARED_ESCROW_SIZE: usize = 8 + 32 + 32 + 2 + 1 + 4 + 64;
 
 // TODO: if size ever changes, be sure to update APPROX_SOL_MARGIN_RENT in tensor-infra
-// seeds: 
+// seeds:
 #[account]
 pub struct SharedEscrow {
     pub owner: Pubkey,
