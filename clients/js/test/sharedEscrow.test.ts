ecd3ee3fbd2f1e2d2af404afd0fabe7781e7b1a6
Samuel Vanderwaal <samuel.vanderwaal@gmail.com>
2024-04-05 14:17:19 -0800

    add a couple example tests

--- clients/js/test/sharedEscrow.test.ts
+++ clients/js/test/sharedEscrow.test.ts
@@ -1,10 +1,4 @@
 import test from 'ava';
-import { generateKeyPairSigner } from '@solana/signers';
-import {
-  Mode,
-  WhitelistV2,
-  fetchWhitelistV2,
-} from '@tensor-foundation/whitelist';
 import { appendTransactionInstruction, none, pipe } from '@solana/web3.js';
 import {
   createDefaultSolanaClient,
@@ -17,51 +11,21 @@ import {
   SharedEscrow,
   fetchPool,
   fetchSharedEscrow,
-  findSharedEscrowPda,
   getAttachPoolToSharedEscrowInstruction,
-  getInitSharedEscrowAccountInstruction,
+  getDepositSharedEscrowAccountInstruction,
 } from '../src/index.js';
-import { createPool, createWhitelistV2 } from './_common.js';
+import { createPoolAndWhitelist } from './_common.js';
+import { initSharedEscrow } from './_sharedEscrow.js';
 
 test('it can create a pool and attach it to a shared escrow', async (t) => {
   const client = createDefaultSolanaClient();
-  const owner = await generateKeyPairSignerWithSol(client);
-  const freezeAuthority = (await generateKeyPairSigner()).address;
-  const namespace = await generateKeyPairSigner();
-
-  // Setup a basic whitelist to use with the pool.
-  const conditions = [{ mode: Mode.FVC, value: owner.address }];
-
-  const { whitelist, uuid } = await createWhitelistV2({
-    client,
-    updateAuthority: owner,
-    freezeAuthority,
-    conditions,
-    namespace,
-  });
-
-  // Then a whitelist was created with the correct data.
-  t.like(await fetchWhitelistV2(client.rpc, whitelist), <WhitelistV2>(<unknown>{
-    address: whitelist,
-    data: {
-      updateAuthority: owner.address,
-      namespace: namespace.address,
-      freezeAuthority,
-      uuid,
-      conditions,
-    },
-  }));
 
   // Create default pool
-  const { pool } = await createPool({
-    client,
-    whitelist,
-    owner,
-  });
+  const { pool, owner } = await createPoolAndWhitelist({ client });
 
   const poolAccount = await fetchPool(client.rpc, pool);
   // Then an account was created with the correct data.
-  t.like(poolAccount, <Pool>(<unknown>{
+  t.like(poolAccount, <Pool>{
     address: pool,
     data: {
       owner: owner.address,
@@ -74,33 +38,16 @@ test('it can create a pool and attach it to a shared escrow', async (t) => {
         mmFeeBps: none(),
       },
     },
-  }));
-
-  const sharedEscrowNr = 0;
-  const name = new Uint8Array(32).fill(1);
-
-  const [sharedEscrow] = await findSharedEscrowPda({
-    owner: owner.address,
-    nr: sharedEscrowNr,
   });
 
-  // Create a shared escrow
-  const initSharedEscrowIx = getInitSharedEscrowAccountInstruction({
+  const { sharedEscrow, name, sharedEscrowNr } = await initSharedEscrow({
+    client,
     owner,
-    sharedEscrow,
-    sharedEscrowNr,
-    name,
   });
-
-  await pipe(
-    await createDefaultTransaction(client, owner.address),
-    (tx) => appendTransactionInstruction(initSharedEscrowIx, tx),
-    (tx) => signAndSendTransaction(client, tx)
-  );
 
   const sharedEscrowAccount = await fetchSharedEscrow(client.rpc, sharedEscrow);
 
-  t.like(sharedEscrowAccount, <SharedEscrow>(<unknown>{
+  t.like(sharedEscrowAccount, <SharedEscrow>{
     address: sharedEscrow,
     data: {
       owner: owner.address,
@@ -108,7 +55,7 @@ test('it can create a pool and attach it to a shared escrow', async (t) => {
       nr: sharedEscrowNr,
       poolsAttached: 0,
     },
-  }));
+  });
 
   const attachIx = getAttachPoolToSharedEscrowInstruction({
     owner,
@@ -134,6 +81,66 @@ test('it can create a pool and attach it to a shared escrow', async (t) => {
       name,
       nr: sharedEscrowNr,
       poolsAttached: 1,
+    },
+  }));
+});
+
+test('it can deposit funds to a shared escrow', async (t) => {
+  const client = createDefaultSolanaClient();
+
+  const owner = await generateKeyPairSignerWithSol(client);
+
+  // Create default shared escrow.
+  const { sharedEscrow, name, sharedEscrowNr } = await initSharedEscrow({
+    client,
+    owner,
+  });
+
+  // Check that the shared escrow was created correctly.
+  const sharedEscrowAccount = await fetchSharedEscrow(client.rpc, sharedEscrow);
+
+  const rentLamports = sharedEscrowAccount.lamports;
+
+  t.like(sharedEscrowAccount, <SharedEscrow>{
+    address: sharedEscrow,
+    data: {
+      owner: owner.address,
+      name,
+      nr: sharedEscrowNr,
+      poolsAttached: 0,
+    },
+  });
+
+  const depositLamports = 10_000_000n;
+
+  const depositIx = getDepositSharedEscrowAccountInstruction({
+    owner,
+    sharedEscrow,
+    lamports: depositLamports,
+  });
+
+  await pipe(
+    await createDefaultTransaction(client, owner.address),
+    (tx) => appendTransactionInstruction(depositIx, tx),
+    (tx) => signAndSendTransaction(client, tx)
+  );
+
+  const updatedSharedEscrowAccount = await fetchSharedEscrow(
+    client.rpc,
+    sharedEscrow
+  );
+
+  t.assert(
+    updatedSharedEscrowAccount.lamports === depositLamports + rentLamports
+  );
+
+  t.like(updatedSharedEscrowAccount, <SharedEscrow>(<unknown>{
+    address: sharedEscrow,
+    data: {
+      owner: owner.address,
+      name,
+      nr: sharedEscrowNr,
+      poolsAttached: 0,
     },
   }));
 });
