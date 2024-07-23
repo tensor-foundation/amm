8301164fd837f1fda1ead5b9670f3e964edb4c41
Samuel Vanderwaal <samv@tensor.so>
2024-07-22 20:35:32 -0800

    add root eslintrc file

---
+++ .eslintrc.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+  root: true,
+  // Base configuration here
+  extends: ["@solana/eslint-config-solana"],
+  rules: {
+    "@typescript-eslint/ban-types": "off",
+    "@typescript-eslint/sort-type-constituents": "off",
+    "prefer-destructuring": "off",
+    "simple-import-sort/imports": "off",
+    "sort-keys-fix/sort-keys-fix": "off",
+    "typescript-sort-keys/interface": "off",
+  },
+};
