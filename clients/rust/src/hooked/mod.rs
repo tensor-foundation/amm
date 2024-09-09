faa5289a9f85987c4207c7edb487148eed4ffddc
Samuel Vanderwaal <samv@tensor.so>
2024-09-08 21:52:19 -0800

    add calc fees

--- clients/rust/src/hooked/mod.rs
+++ clients/rust/src/hooked/mod.rs
@@ -1,9 +1,14 @@
 pub mod currency;
+pub mod fees;
 pub mod nullable;
 pub mod pool;
 
 pub use currency::*;
+pub use fees::*;
 pub use nullable::*;
 
 pub const HUNDRED_PCT_BPS: u16 = 10000;
 pub const LAMPORTS_PER_SOL: u64 = 1_000_000_000;
+pub const BROKER_FEE_PCT: u64 = 50;
+pub const MAKER_BROKER_PCT: u8 = 80;
+pub const TAKER_FEE_BPS: u64 = 200;
