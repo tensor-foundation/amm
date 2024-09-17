181dd1f86667471b7385422e7fad3ad98518e5a8
Samuel Vanderwaal <samv@tensor.so>
2024-09-17 13:35:12 -0800

    add mpl-core test using merkle root whitelist

--- clients/js/test/mpl_core/buyNft.test.ts
+++ clients/js/test/mpl_core/buyNft.test.ts
@@ -183,6 +183,77 @@ test('it can buy an NFT from a NFT pool', async (t) => {
   await assertNftReceiptClosed({ t, client, pool, mint: asset.address });
 });
 
+test('it can buy an NFT from a Trade pool w/ Merkle Root whitelist', async (t) => {
+  const { client, signers, asset, collection, testConfig, pool, feeVault } =
+    await setupCoreTest({
+      t,
+      poolType: PoolType.Trade,
+      action: TestAction.Buy,
+      whitelistMode: Mode.MerkleTree,
+      useSharedEscrow: false,
+      fundPool: false,
+    });
+
+  const { buyer, poolOwner, nftUpdateAuthority } = signers;
+  const { poolConfig } = testConfig;
+
+  // Max amount is the maximum price the user is willing to pay for the NFT + creators fee and mm fee, if applicable.
+  const mmFee = poolConfig.startingPrice * BigInt(poolConfig.mmFeeBps ?? 0);
+  const royalties = (poolConfig.startingPrice * 500n) / 10000n;
+  // It should work with exact amount, but users might also pad this to allow for slippage.
+  const maxAmount = poolConfig.startingPrice + mmFee + royalties;
+
+  const startingFeeVaultBalance = (await client.rpc.getBalance(feeVault).send())
+    .value;
+
+  // Buy NFT from pool
+  const buyNftIx = await getBuyNftCoreInstructionAsync({
+    owner: poolOwner.address,
+    buyer,
+    pool,
+    asset: asset.address,
+    collection: collection.address,
+    maxAmount,
+    // Remaining accounts
+    creators: [nftUpdateAuthority.address],
+  });
+
+  await pipe(
+    await createDefaultTransaction(client, buyer),
+    (tx) => appendTransactionMessageInstruction(buyNftIx, tx),
+    (tx) => signAndSendTransaction(client, tx)
+  );
+
+  // NFT is now owned by the buyer.
+  t.like(await fetchAssetV1(client.rpc, asset.address), <
+    Account<AssetV1, Address>
+  >{
+    address: asset.address,
+    data: {
+      owner: buyer.address,
+    },
+  });
+
+  // Pool stats are updated
+  t.like(await fetchPool(client.rpc, pool), <Account<Pool, Address>>{
+    address: pool,
+    data: {
+      stats: {
+        takerBuyCount: 1,
+        takerSellCount: 0,
+      },
+    },
+  });
+
+  // Fee vault balance increases.
+  const endingFeeVaultBalance = (await client.rpc.getBalance(feeVault).send())
+    .value;
+  t.assert(endingFeeVaultBalance > startingFeeVaultBalance);
+
+  // Deposit Receipt is closed
+  await assertNftReceiptClosed({ t, client, pool, mint: asset.address });
+});
+
 test('buying NFT from a trade pool increases currency amount', async (t) => {
   const { client, signers, asset, collection, testConfig, pool } =
     await setupCoreTest({
